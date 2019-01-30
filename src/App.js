0c88822d2ff5421b7b14833522476427f1b933d7
YUXIN LI <liyuxin2017@126.com>
2019-01-30 14:35:56 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
+
 import './App.css';
 import Article from './Article';
 import MarkedUpArticle from './MarkedUpArticle';
@@ -12,26 +12,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Article
-            title = "The Statue of Liberty's torch heads to new museum"
-            date = "November 22, 2018"
-            author = "Kate Farley">
-            <MarkedUpArticle/>
-          </Article>
-=======
-import List from './List';
-import AddingMachine from './AddingMachine';
-import ArticleTable from './ArticleTable';
-import articles from './data/articles.json';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ArticleTable articles={articles} />
->>>>>>> 7fe78b595b740273d50e2c87b3b15ea60ef2e2c9
+        <Article
+          title = "The Statue of Liberty's torch heads to new museum"
+          date = "November 22, 2018"
+          author = "Kate Farley">
+          <MarkedUpArticle/>
+        </Article>
       </div>
-    );
+    )
   }
 }
 
