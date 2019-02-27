9faeb46800c4ecbbe004acf0af6a5212eced9292
YUXIN LI <liyuxin2017@126.com>
2019-02-27 15:49:45 -0800

    Week 3 assignment revaluation

--- src/Article.js
+++ src/Article.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Article = ({title, date, author, children}) => {
   return (
-    <div>
+    <Article>
       <h1 id = 'title'>{title}</h1>
-      <h2 id = 'author'>{author}</h2>
-      <h3 id = 'date'>{date}</h3>
+      <h2 id = 'author'><address>{author}</address></h2>
+      <h3 id = 'date'><time>{date}</time></h3>
       <hr/>
       {children}
-    </div>
+    </Article>
   );
 };
 
